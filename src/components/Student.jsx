5500a2630ff2b1de9cd9335ecf780a6304e4982a
Ansel Rognlie <ansel.rognlie@gmail.com>
2024-06-25 12:51:09 -0700

    basic component body

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,34 +1,5 @@
-import './Student.css';
-import PropTypes from 'prop-types';
-
-const Student = (props) => {
-  const attendanceButtonClicked = () => {
-    // Invoke the function passed in through the prop named "onPresenceToggle"
-    // This function refers to the toggleStudentPresence function in App
-    props.onPresenceToggle(props.id);
-};
-
-  const nameColor = props.isPresent ? 'green' : 'red';
-
-  return (
-    <div className="Student">
-      <ul>
-        <li className={nameColor}>Nickname: {props.name}</li>
-        <li>Email: {props.email}</li>
-      </ul>
-      <button onClick={attendanceButtonClicked}>
-        Toggle if {props.name} is present
-      </button>
-    </div>
-  );
-};
-
-Student.propTypes = {
-  id: PropTypes.number.isRequired,
-  name: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-  isPresent: PropTypes.bool.isRequired,
-  onPresenceToggle: PropTypes.func.isRequired,
+const Student = () => {
+  return "student";
 };
 
 export default Student;
