806437cbe8e8269ecdd4210e5d7a5ef4b1f51745
Ansel Rognlie <ansel.rognlie@gmail.com>
2024-06-25 13:41:00 -0700

    excludes main.jsx from coverage report

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { configDefaults } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -10,5 +11,8 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./vitest.setup.js'],
+    coverage: {
+      exclude: [...configDefaults.coverage.exclude, 'src/main.jsx'],
+    },
   },
 });
